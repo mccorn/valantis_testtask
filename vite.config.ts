4523444159cad4de9962c306f4e7f7ea54c416ba
Markeev Andrew <mccorn13@gmail.com>
2024-02-26 13:10:36 +0300

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/valantis_testtask",
+  base: "https://github.com/mccorn/valantis_testtask",
   plugins: [react()],
 })
